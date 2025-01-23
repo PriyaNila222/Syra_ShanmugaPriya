28c96200ba8b072365e80984ad6bacdbdfc3ad1f
PriyaNila222 <priyark1991@gmail.com>
2025-01-23 13:36:15 +0530

    Syra_Assignment

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:ps-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />              
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
